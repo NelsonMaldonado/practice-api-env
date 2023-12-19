ff5c8b26795683d7dbf9add071aafdc4363d7a84
Nelson Maldonado <play2nino@gmail.com>
2023-12-18 20:01:48 -0800

    added env file

--- src/components/youtubeCounter.js
+++ src/components/youtubeCounter.js
@@ -2,40 +2,27 @@ import React, { useEffect } from "react"
 import axios from "axios"
 import { useState } from "react"
 
+const youtubekey = ""
+const youtubeUser = ""
+
 const YoutubeCounter = () => {
-  const [data, setData] = useState(
-    "https://images.dog.ceo/breeds/terrier-welsh/lucy.jpg"
-  )
-
-  //functions starts here
-  // useEffect(() => {
-  //   const getData = () => {
-  //     axios
-  //       .get("https://dog.ceo/api/breeds/image/random/")
-
-  //       .then((res) => console.log(res))
-  //       .catch((err) => console.log(err.message))
-  //   }
-  //   getData()
-  // }, [])
-  //Functions start here
+  const [viewCount, setViewCount] = useState(0)
+  const [channelName, setChannelName] = useState("Youtube Channel")
   const fetchData = () => {
-    //https://pokeapi.co/api/v2/pokemon/ditto
-    fetch("https://dog.ceo/api/breeds/image/random")
+    fetch()
       .then((res) => res.json())
-      .then((res) => setData(res.message))
 
-      .catch((err) => console.error(err))
+      .catch((err) => console.log(err))
   }
 
   return (
     <div>
       <h1>Youtube Counter</h1>
       {/* <p>{data}</p> */}
-      <h4>Press the button to display random animals</h4>
-      <button onClick={fetchData}>Fetch APIs</button>
+      <h5>Press the button to display Subscribers</h5>
+      <button onClick={fetchData}>Subscribers Counter</button>
       <div>
-        <img src={data} alt="Random_dog" />
+        <h3>{channelName}</h3>
       </div>
     </div>
   )
