27e32f6e38b64653371708037ae19b9f0b75b0c3
Nelson Maldonado <play2nino@gmail.com>
2023-12-18 23:35:21 -0800

    fetching youtube API working

--- src/components/youtubeCounter.js
+++ src/components/youtubeCounter.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react"
-import axios from "axios"
+import React from "react"
+
 import { useState } from "react"
 
-const youtubekey = ""
-const youtubeUser = ""
+const youtubekey = "AIzaSyAFOnW68MXTHQ-iR0vikLhb9Q6oVdIOnvw"
+const youtubeUser = "UClTIFAW9Nj-bjRdxCuvYkuQ"
 
 const YoutubeCounter = () => {
-  const [viewCount, setViewCount] = useState(0)
-  const [channelName, setChannelName] = useState("Youtube Channel")
+  const [subscriberCount, setsubscriberCount] = useState(0)
+  const [totalViews, settotalViews] = useState(0)
   const fetchData = () => {
-    fetch()
+    fetch(
+      `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${youtubeUser}&key=${youtubekey}`
+    )
       .then((res) => res.json())
+      .then((data) => {
+        const subscriberCount = data.items[0].statistics.subscriberCount
+        const views = data.items[0].statistics.viewCount
+        const parse = parseInt(views)
+        const formattedViews = parse.toLocaleString(Intl.NumberFormatOptions)
+        console.log(formattedViews)
+        setsubscriberCount(subscriberCount)
+        settotalViews(formattedViews)
+      })
 
       .catch((err) => console.log(err))
   }
@@ -20,9 +31,15 @@ const YoutubeCounter = () => {
       <h1>Youtube Counter</h1>
       {/* <p>{data}</p> */}
       <h5>Press the button to display Subscribers</h5>
-      <button onClick={fetchData}>Subscribers Counter</button>
+      <button onClick={fetchData}>Fetch API</button>
       <div>
-        <h3>{channelName}</h3>
+        <h3>Subscribers</h3>
+        <h3 style={{ color: "#83f28f" }}>{subscriberCount}</h3>
+        <h2>Total Views</h2>
+        <h3 style={{ color: "#83f28f" }}>
+          {totalViews}
+          <code style={{ color: "#83f28f" }}>!</code>
+        </h3>
       </div>
     </div>
   )
